a057c902518a20a4868e9b248c0162ff3a0f6ec6
BumbleVeee <gubek.veronika.eva@gmail.com>
2026-02-28 21:24:24 +0100

    javitott feladatok, utolso harom feladat javitasra var

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ window.addEventListener("load", function () {
     elemekElerese2();
     elelmekElerese3();
     elelmekElerese4();
-   
+    elemekFormazasa();
+    esemenyKezeles1();
+    esemenyKezeles2();
+    esemenyKezeles3(feladatElem);
+
 });
 
 function elemekElerese1() {
@@ -31,4 +35,45 @@ function elelmekElerese4() {
     }
     szoveg +="</ul>"
     listaElem.innerHTML=szoveg
+}
+
+function elemekFormazasa() {
+    const listaElem=document.querySelectorAll(".lista") [0]
+    listaElem.classList.add("formazott");
+}
+
+function esemenyKezeles1() {
+    const listaElem=document.querySelectorAll(".lista") [0]
+    const kattintasUtan = document.querySelectorAll(".kattintasutan")[0];
+
+    listaElem.addEventListener("click", function () {
+        kattintasUtan.innerHTML = listaElem.innerHTML;
+    });
+
+}
+
+function esemenyKezeles2() {
+    const feladatElem=document.querySelectorAll(".feladat") [0]
+    feladatElem.innerHTML="<button>OK</button>";
+    const gombElem=document.querySelectorAll(".feladat button") [0];
+    
+    gombElem.addEventListener("click", function () {
+        feladatElem.innerHTML +=`<div>
+        <img src="https://paradepets.com/.image/w_640,q_auto:good,c_limit/NTowMDAwMDAwMDAwMDQyNDA2/smoke-black-maine-coon.jpg?arena_f_auto" alt="random kép"></div>`;
+    });
+
+    const kepElem = feladatElem.querySelector("img");
+
+    esemenyKezeles3(kepElem);
+}
+
+function esemenyKezeles3(kepElem) {
+
+    kepElem.addEventListener("mouseover", function () {
+        kepElem.style.transform = "scale(1.2)";
+    });
+
+    kepElem.addEventListener("mouseout", function() {
+        kepElem.style.transform = "scale(1)";
+    })
 }
